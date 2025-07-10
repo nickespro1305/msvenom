4e7939f8a8ffc5fe6868343a04a146ed9904c9d2
nickespro130 <nickespro130@outlook.es>
2025-07-10 19:05:02 +0200

    v2.1 implementacion temprana de make 2

--- installer.py
+++ installer.py
@@ -7,11 +7,22 @@ def install():
     
     os.mkdir("apps")
     os.mkdir("keys")
+    os.mkdir("lib")
 
     subprocess.run("curl -O https://raw.githubusercontent.com/nickespro1305/msvenom/refs/heads/main/main.py", shell=True, check=True)
 
     os.chdir("keys")
     subprocess.run("curl -o sources.json https://raw.githubusercontent.com/nickespro1305/msvenom/refs/heads/main/SERVER/default_sources.json", shell=True, check=True)
+    os.chdir("..")
+
+    os.chdir("lib")
+    os.mkdir("make")
+    os.chdir("make")
+    subprocess.run("curl -o make.zip https://raw.githubusercontent.com/nickespro1305/msvenom/refs/heads/main/SERVER/DEPENDENCES/make-3.81-bin.zip", shell=True, check=True)
+    subprocess.run("curl -o makeD.zip https://raw.githubusercontent.com/nickespro1305/msvenom/refs/heads/main/SERVER/DEPENDENCES/make-3.81-dep.zip", shell=True, check=True)
+    subprocess.run("tar -xf make.zip")
+    subprocess.run("tar -xf makeD.zip")
+    os.chdir("..")
     os.chdir("..")
 
     subprocess.run("pip install rich requests", shell=True, check=True)
